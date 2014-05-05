fee2c0e2c6dc9708f7b0feaf5270c45d83d37b38
Bo Liu <bo658713@gmail.com>
2014-05-04 18:40:00 -0700

    Fix exit price and sell at end

--- stock.py
+++ stock.py
@@ -7,13 +7,15 @@ from config import Config
 from last_n_high import LastNHigh
 
 class _Transaction:
-  def __init__(self, parsed_row, factor, average_true_range):
+  def __init__(self, parsed_row, factor, average_true_range, exit_price):
     self.buy_row = parsed_row
     self.buy_factor = factor
     self.average_true_range = average_true_range
+    self.exit_price = exit_price
 
-  def exit_price(self):
-    return self.average_true_range * Config.M
+  def update_exit_price(self, exit_price):
+    if exit_price > self.exit_price:
+      self.exit_price = exit_price
 
   def sell(self, sell_row, sell_factor):
     print self.buy_row.price * self.buy_factor, '->',\
@@ -28,9 +30,10 @@ class Stock:
   def __init__(self, first_row):
     self._permno_str = first_row.permno
     self._first_row = ParsedFirstRow(first_row)
+    self._last_row = None
 
     self._state = Stock._PENDING
-    self._buy = None
+    self._transaction = None
     self._factor = 1.0
     self._adj_price = 0
 
@@ -48,6 +51,7 @@ class Stock:
     assert(row.date >= self._first_row.date)
     if not row.parsed:
       return
+    self._last_row = row
 
     self._factor = self._factor * (1.0 + row.facpr)
     adj_price = row.price * self._factor
@@ -66,6 +70,7 @@ class Stock:
     self._last_d_true_range_sum += true_range
     average_true_range =\
         self._last_d_true_range_sum / len(self._last_true_range)
+    exit_price = adj_price - average_true_range * Config.M
 
     high_price = sys.maxint
     if row.date - self._first_row.date >= self._n_time_delta:
@@ -76,18 +81,18 @@ class Stock:
       if adj_price > high_price:
         self._state = Stock._BUY_TOMORROW
     elif self._state == Stock._BUY_TOMORROW:
-      self._buy = _Transaction(row, self._factor, average_true_range)
+      self._transaction = _Transaction(row, self._factor, average_true_range, exit_price)
       self._state = Stock._BOUGHT
     elif self._state == Stock._BOUGHT:
-      if average_true_range > self._buy.average_true_range:
-        self._buy.average_true_range = average_true_range
-      if adj_price <= self._buy.exit_price():
+      self._transaction.update_exit_price(exit_price)
+      if adj_price <= self._transaction.exit_price:
         self._state = Stock._SELL_TOMORROW
     else:
       assert self._state == Stock._SELL_TOMORROW
-      self._buy.sell(row, self._factor)
-      self._buy = None
-      self._state = Stock._PENDING
+      self.done()
 
   def done(self):
-    print self._permno_str, self._factor
+    if self._transaction:
+      self._transaction.sell(self._last_row, self._factor)
+      self._transaction = None
+      self._state = Stock._PENDING
